7d264a727e891f8b3ad6b9c3ffd4380bc3c0f008
Lucas Lafèré <laferelcas@gmail.com>
2022-04-01 16:47:44 -0300

    botao fechar pedido funcional

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 
+
+
+
+function toggleBotao (element) {
+    console.log(element);
+    let selecionarComida = document.querySelector(".comida .selecionado");
+
+    let selecionarBebida = document.querySelector(".bebida .selecionado");
+
+    let selecionarSobremesa = document.querySelector(".sobremesa .selecionado");
+
+    let botao = document.querySelector(".aceso")
+
+    if (selecionarComida !== null && selecionarBebida !== null && selecionarSobremesa !== null) {
+        botao.classList.remove("escondido")
+    }
+}
+
 //funçao que seleciona uma comida e adiciona a classe selecionado quando clica
 //e desmarca os já clicados
 
-
 function toggleComida (element) {
+    
     console.log(element);
     const selecionar = document.querySelector(".selecionado");
     if (selecionar !== null) {
         selecionar.classList.remove("selecionado")
     }
     element.classList.add("selecionado")
+
+    toggleBotao();
+    
 }
 
 //funcao que seleciona bebida e desmarca ja clicados
@@ -20,6 +41,10 @@ function toggleBebida (element) {
         selecionar.classList.remove("selecionado")
     }
     element.classList.add("selecionado")
+    
+    toggleBotao();
+
+
 }
 
 //funcao que seleciona sobremesa e desmarca ja clicados
@@ -30,6 +55,13 @@ function toggleSobremesa (element) {
         selecionar.classList.remove("selecionado")
     }
     element.classList.add("selecionado")
+
+    toggleBotao();
 }
 
+//checkmark?
 
+
+// fazer o botao de pedido com tipo:
+//if comida, bebida, sobremesa contains selecionado, o botao funciona 
+//add class? add onclick?
