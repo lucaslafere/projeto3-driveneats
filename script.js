58d294fd2fab22428d9abe5083738c418001d8b8
Lucas Lafèré <laferelcas@gmail.com>
2022-04-02 16:30:18 -0300

    versao final sem bonus, pedido wpp funcionando com mensagem e valor total

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-
-
-
+let custoPrato;
+let custoBebida;
+let custoSobremesa;
+let custoTotal;
 
 function toggleBotao (element) {
     console.log(element);
@@ -29,8 +30,30 @@ function toggleComida (element) {
     }
     element.classList.add("selecionado")
 
+//parte que checa se o botao de fechar pedido deve aparecer
     toggleBotao();
-    
+
+//parte feita pra checar valor da comida
+
+    const comidaEscolhida = document.querySelector(".comida .selecionado").querySelector("h4").innerHTML;
+
+    if (comidaEscolhida === "Frango Yin Yang") {
+        custoPrato = 14.90;
+    }
+    else if (comidaEscolhida === "Macarronada") {
+        custoPrato = 16.90;
+    }
+    else if (comidaEscolhida === "Strong on off") {
+        custoPrato = 15.90;
+    }
+    else if (comidaEscolhida === "Lasanha") {
+        custoPrato = 13.90;
+    }
+    else if (comidaEscolhida === "Quichic") {
+        custoPrato = 15.00;
+    }
+    custoTotal = custoPrato + custoBebida + custoSobremesa;
+
 }
 
 //funcao que seleciona bebida e desmarca ja clicados
@@ -41,9 +64,30 @@ function toggleBebida (element) {
         selecionar.classList.remove("selecionado")
     }
     element.classList.add("selecionado")
-    
+
+//parte que checa se o botao de fechar pedido deve aparecer
     toggleBotao();
 
+//parte feita pra checar valor da bebida
+
+    const bebidaEscolhida = document.querySelector(".bebida .selecionado").querySelector("h4").innerHTML;
+
+    if (bebidaEscolhida === "Coquinha gelada") {
+        custoBebida = 4.90;
+    }
+    else if (bebidaEscolhida === "Fanta laranja") {
+        custoBebida = 4.50;
+    }
+    else if (bebidaEscolhida === "Guaranazin") {
+        custoBebida = 3.90;
+    }
+    else if (bebidaEscolhida === "Suquinho") {
+        custoBebida = 3.50;
+    }
+    else if (bebidaEscolhida === "Matte imposto") {
+        custoBebida = 4.50;
+    }
+    custoTotal = custoPrato + custoBebida + custoSobremesa;
 
 }
 
@@ -56,12 +100,44 @@ function toggleSobremesa (element) {
     }
     element.classList.add("selecionado")
 
+//parte que checa se o botao de fechar pedido deve aparecer
+
     toggleBotao();
+
+//parte feita pra checar valor da sobremesa
+
+    const sobremesaEscolhida = document.querySelector(".sobremesa .selecionado").querySelector("h4").innerHTML;
+
+
+    if (sobremesaEscolhida === "Pudim") {
+    custoSobremesa = 7.90;
+    }
+    else if (sobremesaEscolhida === "Petit Gateau") {
+    custoSobremesa = 13.90;
+    }
+    else if (sobremesaEscolhida === "Tortinha de limão") {
+    custoSobremesa = 9.90;
+    }
+    else if (sobremesaEscolhida === "Mousse") {
+    custoSobremesa = 6.90;
+    }
+    else if (sobremesaEscolhida === "Pudim") {
+    custoSobremesa = 7.90;
+    }
+    custoTotal = custoPrato + custoBebida + custoSobremesa;
+
 }
 
-//checkmark?
+//Enviar pedido wpp
 
+function enviarPedido (el) {
+    custoTotal = custoPrato + custoBebida + custoSobremesa;
+    const comidaEscolhida = document.querySelector(".comida .selecionado").querySelector("h4").innerHTML;
+    const bebidaEscolhida = document.querySelector(".bebida .selecionado").querySelector("h4").innerHTML;
+    const sobremesaEscolhida = document.querySelector(".sobremesa .selecionado").querySelector("h4").innerHTML;
 
-// fazer o botao de pedido com tipo:
-//if comida, bebida, sobremesa contains selecionado, o botao funciona 
-//add class? add onclick?
+    let mensagem = `Olá, gostaria de fazer o pedido: \n - Prato: ${comidaEscolhida} \n - Bebida: ${bebidaEscolhida} \n - Sobremesa: ${sobremesaEscolhida} \n Total: R$ ${custoTotal.toFixed(2)}`
+
+    window.open("https://wa.me/5521995313835?text=" + encodeURIComponent(mensagem));
+}
+
